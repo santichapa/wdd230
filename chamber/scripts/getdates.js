87f2d52e6e059535e55bc4f31200ad7681e41f11
santichapa <santi.chaparro280@gmail.com>
2024-03-02 20:13:47 -0300

    finished chamber join site

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -1,3 +1,9 @@
 document.getElementById("currentdate").innerText = new Date().getFullYear();
 
 document.querySelector("#lastModified").innerHTML = `Last modified: ${document.lastModified}`;
+
+const recordDateTime = document.querySelector("#recorddt");
+
+recordDateTime.value = Date.now();
+
+localStorage.setItem("recordDateTime-ls", recordDateTime);
