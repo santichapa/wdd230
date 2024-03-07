c6da3d752375113be72ddf18f5c338db316d1c2d
santichapa <santi.chaparro280@gmail.com>
2024-03-07 13:30:18 -0300

    finished learning activities

---
+++ lesson09/scripts/prophets.js
@@ -0,0 +1,32 @@
+const url = "https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json";
+const cards = document.querySelector("#cards");
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    // console.table(data.prophets); // to check the data 
+    displayProtphets(data.prophets);
+}
+
+getProphetData();
+
+const displayProtphets = (prophets) => {
+    prophets.forEach(p => {
+        const card = document.createElement("section");
+        const fullName = document.createElement("h2");
+        const portrait = document.createElement("img");
+
+        fullName.textContent = `${p.name} ${p.lastname}`;
+
+        portrait.setAttribute("src", p.imageurl);
+        portrait.setAttribute("alt", `A picture of ${p.name} ${p.lastname}`);
+        portrait.setAttribute("loading", "lazy");
+        portrait.setAttribute("width", "300");
+        portrait.setAttribute("height", "auto");
+        
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+        cards.appendChild(card);
+    });
+}
