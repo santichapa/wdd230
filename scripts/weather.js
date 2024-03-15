fd2f9680eca128fca57986e3c84e1d0a1543c401
santichapa <santi.chaparro280@gmail.com>
2024-03-15 15:55:53 -0300

    fixed img

--- scripts/weather.js
+++ scripts/weather.js
@@ -7,7 +7,7 @@ async function apiFetch() {
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
-            console.log(data); // testing
+            // console.log(data); // testing
             displayResults(data);
         } else {
             throw Error(await response.text()); 
