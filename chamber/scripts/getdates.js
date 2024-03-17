fc8847c1ace89de1adbad97bc2bcd986f48fbd68
santichapa <santi.chaparro280@gmail.com>
2024-03-16 23:05:41 -0300

    finished banner for home page

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -7,3 +7,19 @@ document.getElementById("currentdate").innerText = new Date().getFullYear();
 // recordDateTime.value = Date.now();
 
 // localStorage.setItem("recordDateTime-ls", recordDateTime);
+
+const btn = document.querySelector("#banner-button"); 
+
+btn.addEventListener("click", () => {
+    document.querySelector(".banner").classList.add("hide");
+});
+
+function checkMeetingDay() {
+    const d = new Date();
+    let day = d.getDay();
+    if (day > 0 && day < 4) {
+        document.querySelector(".banner").classList.remove("hide");
+    }
+}
+
+checkMeetingDay();
